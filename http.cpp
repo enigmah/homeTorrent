a2de5ca0776e3799d12419aa8eb57150bebd4371
dex_Home <hypoadm@gmail.com>
2011-11-26 18:11:30 +0200

    boost asio test
    Signed-off-by: dex_Home <hypoadm@gmail.com>

---
+++ http.cpp
@@ -0,0 +1,13 @@
+#include "http.h"
+#include <iostream>
+
+Http::Http(): asd("Tralala")
+{}
+
+Http::~Http()
+{}
+
+void Http::print()
+{
+    std::cout<< asd;
+}
---
+++ http.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Http
+{
+public:
+    Http();
+    virtual ~Http();
+
+
+    void print();
+
+private:
+    std::string asd;
+};
